93e199242cd5e9cb16aed40a33fddcd50245b147
Gautam Mishra <mishragautam96@gmail.com>
2021-06-24 20:35:08 +0530

    add utils and exceptions

---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    DATABASE_PATH = './database/'
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
---
+++ app/core/exceptions.py
@@ -0,0 +1,10 @@
+class DatabaseError(Exception):
+    pass
+
+
+class NotSupportedError(Exception):
+    pass
+
+
+class OperationalError(Exception):
+    pass
