becb37cf7f1c5f7e8d028b5c9f26ee4b459a0915
Gautam Mishra <mishragautam96@gmail.com>
2021-07-15 22:35:50 +0530

    add queries

--- app/providers.sql
+++ app/providers.sql
@@ -1,9 +1,22 @@
 -- name: get_all_providers
-select * from providers;
+select * from providers
+limit :limit offset :offset;
 
--- name: delete_all_providers
-delete from providers
-returning *;
+-- name: get_provider_by_id
+select * from providers
+    where id = :providerid;
+
+-- name: get_phone_by_provider_id
+select phone from providers
+    where id = :providerid;
+
+-- name: get_speclialty_by_provider_id
+select specialty from providers
+    where id = :providerid;
+
+-- name: get_qualification_by_provider_id
+select qualification from providers
+    where id = :providerid;
 
 -- name: create_provider
 insert into providers(providerid, active, name, organization, address, department, location)
@@ -28,3 +41,17 @@ insert into specialities(providerid, speciality)
 values (:providerid, :speciality)
 RETURNING
     providerid, speciality;
+
+-- name: update_provider
+update providers
+    set active = :active,
+        name = :name,
+        organization = :organization,
+        address = :address,
+        department = :department,
+        location = :location
+    where id = :providerid;
+
+-- name: delete_all_providers
+delete from providers
+returning *;
